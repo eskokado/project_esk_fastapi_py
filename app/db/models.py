62c5153d534ebaf4d480cc8681b7c3d04c2f65ca
eskokado <eskokado@gmail.com>
2023-05-01 11:36:37 -0300

    feat: create Product model

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.db.base import Base
 
 
@@ -7,3 +10,18 @@ class Category(Base):
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     name = Column('name', String, nullable=False)
     slug = Column('slug', String, nullable=False)
+    products = relationship('Product', back_populates='category')
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String, nullable=False)
+    slug = Column('slug', String, nullable=False)
+    price = Column('price', Float)
+    stock = Column('stock', Integer)
+    created_at = Column('created_at', DateTime, default=datetime.now())
+    updated_at = Column('updated_at', DateTime, onupdate=datetime.now())
+
+    category_id = Column('category_id', ForeignKey('categories.id'), nullable=False)
+    category = relationship('Category', back_populates='products')
