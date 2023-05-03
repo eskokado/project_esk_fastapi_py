451f8df09d1394a29dad623fa5c3b02fbe1d2120
eskokado <eskokado@gmail.com>
2023-05-03 05:15:38 -0300

    feat: create fixture product_on_db

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -35,3 +35,26 @@ def categories_on_db(db_session):
         db_session.delete(category)
     db_session.commit()
 
+
+@pytest.fixture()
+def product_on_db(db_session):
+    category = CategoryModel(name="Roupa", slug="roupa")
+    db_session.add(category)
+    db_session.commit()
+
+    product = ProductModel(
+        name="Camisa Abidas",
+        slug="camisa-abidas",
+        price=100.99,
+        stock=20,
+        category_id=category.id
+    )
+
+    db_session.add(product)
+    db_session.commit()
+
+    yield product
+
+    db_session.delete(product)
+    db_session.delete(category)
+    db_session.commit()
