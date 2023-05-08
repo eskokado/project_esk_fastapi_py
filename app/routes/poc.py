255ef03edbd719d398f0b9507d5b7beb21aa7e67
eskokado <eskokado@gmail.com>
2023-05-08 06:05:17 -0300

    refactor: fastapi pagination

--- app/routes/poc.py
+++ app/routes/poc.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.routes.deps import get_db_session
 from app.schemas.category import CategoryOutput
 from app.db.models import Category as CategoryModel
+from app.use_cases.poc import list_categories_uc
 from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage, Params, LimitOffsetParams
 from fastapi_pagination.ext.sqlalchemy import paginate as sqlalchemy_paginate
 
@@ -11,12 +12,8 @@ router = APIRouter(prefix="/pocs", tags=['POC'])
 
 @router.get('/list', response_model=Page[CategoryOutput])
 @router.get('/list/limit-offset', response_model=LimitOffsetPage[CategoryOutput])
-def list_categories(page: int = 1, size: int = 50):
-    categories = [
-        CategoryOutput(name=f'category {n}', slug=f'category-{n}', id=n) for n in range(100)
-    ]
-    params = Params(page=page, size=size)
-    return paginate(categories, params=params)
+def list_categories(page: int = 1, size: int = 50, limit: int = 50, offset: int = 0):
+    return list_categories_uc(page=page, size=size)
 
 
 add_pagination(router)
