193c6390e103e93173edd91a2ee1c6095fc16f52
eskokado <eskokado@gmail.com>
2023-05-08 05:56:47 -0300

    feat: add params in the poc fastapi pagination with sqlalchemy

--- app/routes/poc.py
+++ app/routes/poc.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.routes.deps import get_db_session
 from app.schemas.category import CategoryOutput
 from app.db.models import Category as CategoryModel
-from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage
+from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage, Params, LimitOffsetParams
 from fastapi_pagination.ext.sqlalchemy import paginate as sqlalchemy_paginate
 
 router = APIRouter(prefix="/pocs", tags=['POC'])
@@ -11,12 +11,12 @@ router = APIRouter(prefix="/pocs", tags=['POC'])
 
 @router.get('/list', response_model=Page[CategoryOutput])
 @router.get('/list/limit-offset', response_model=LimitOffsetPage[CategoryOutput])
-def list_categories():
+def list_categories(page: int = 1, size: int = 50):
     categories = [
         CategoryOutput(name=f'category {n}', slug=f'category-{n}', id=n) for n in range(100)
     ]
-
-    return paginate(categories)
+    params = Params(page=page, size=size)
+    return paginate(categories, params=params)
 
 
 add_pagination(router)
