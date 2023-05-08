28bfa13b783c3dc428990fb7754d0f9fcc0b6eea
eskokado <eskokado@gmail.com>
2023-05-08 05:16:16 -0300

    feat: add poc fastapi pagination with categories

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from app.routes.category_routes import router as category_routes
 from app.routes.product_routes import router as product_routes
 from app.routes.user_routes import router as user_routes
+from app.routes.poc import router as poc_routes
 
 app = FastAPI()
 
 app.include_router(category_routes)
 app.include_router(product_routes)
 app.include_router(user_routes)
+app.include_router(poc_routes)
 
 
 @app.get('/health-check')
