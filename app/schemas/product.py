dce94e2e734758b0fa848ddd72fa15307dbfcfea
eskokado <eskokado@gmail.com>
2023-05-09 06:29:24 -0300

    fix: Category to CategoryOutput into Product

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -3,7 +3,8 @@ import re
 from pydantic import validator
 
 from app.schemas.base import CustomBaseModel
-from app.schemas.category import Category
+from app.schemas.category import CategoryOutput
+
 
 class Product(CustomBaseModel):
     name: str
@@ -31,4 +32,7 @@ class ProductInput(CustomBaseModel):
 
 class ProductOutput(Product):
     id: int
-    category: Category
+    category: CategoryOutput
+
+    class Config:
+        orm_mode: True
